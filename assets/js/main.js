21ba6de39f8c8130db2fb9f0e508b584a5a33d52
Lucas Silva <lucas@greenlegis.com.br>
2022-12-04 02:40:16 -0300

    inclusão NavBar Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,47 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 /*=============== SHOW MENU ===============*/
-
+/* validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 /*============== MENU HIDDEN ===============*/
+/* validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav-link");
 
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // when we click on each nav link, we remove the show menu class
+  navMenu.classList.remove("show-menu");
+}
+
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // when the scroll is greater than 80 viewport height, add the class scroll header to the tag header
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 var swiper = new Swiper(".testimonial-wrapper", {
-    loop: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        
-    },
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
